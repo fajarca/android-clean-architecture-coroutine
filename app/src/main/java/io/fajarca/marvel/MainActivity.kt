2883f761f24530876b9b4aeaa98e4848e34c8030
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2020-02-04 11:24:15 +0700

    Setup bottomnav

--- app/src/main/java/io/fajarca/marvel/MainActivity.kt
+++ app/src/main/java/io/fajarca/marvel/MainActivity.kt
@@ -2,6 +2,10 @@ package io.fajarca.marvel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bottomNavigation, navHostFragment.findNavController())
     }
 }

341dec22c25499cfe4ceae677546e5bc03f69a24
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2020-02-08 21:01:11 +0700

    Remove unused import

--- app/src/main/java/io/fajarca/marvel/MainActivity.kt
+++ app/src/main/java/io/fajarca/marvel/MainActivity.kt
@@ -1,18 +1,9 @@
 package io.fajarca.marvel
 
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
